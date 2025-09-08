f92176a20b58528bbc3e8ca83344e8c55e148c15
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-08 14:24:14 +0530

    initialized

---
+++ TypeScript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 25;
+let name = "John Doe";
+let isStudent = true;
+console.log(`Name: ${name}, Age: ${age}, Is Student: ${isStudent}`);
+// npx tsc 
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/index.ts
@@ -0,0 +1,21 @@
+// number, string, boolean
+let age : number = 25;
+let name : string = "John Doe";
+let isStudent : boolean = true;
+
+// if we cant determine the type, we can use 'any'
+let anything: any = "Could be anything";
+anything = 42; // Now it's a number
+
+// arrays
+let numbers: number[] = [1, 2, 3, 4, 5];
+let names: string[] = ["Alice", "Bob", "Charlie"];
+let flags: boolean[] = [true, false, true];
+
+// tuples
+let person: [string, number] = ["Alice", 30];
+
+
+console.log(`Name: ${name}, Age: ${age}, Is Student: ${isStudent}`);
+
+// npx tsc 
