4d7750bd4d3f6f9af633dbe9d1481d006c938a7a
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-08 14:39:22 +0530

    initialized

---
+++ Javascript/index.js
@@ -0,0 +1,5 @@
+let me = "sasuni";
+let age = 21;
+let isStudent = true;
+
+console.log(`My name is ${me}. I am ${age} years old. Student status: ${isStudent}`);
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -3,6 +3,8 @@ let age : number = 25;
 let name : string = "John Doe";
 let isStudent : boolean = true;
 
+console.log(`Name: ${name}, Age: ${age}, Is Student: ${isStudent}`);
+
 // if we cant determine the type, we can use 'any'
 let anything: any = "Could be anything";
 anything = 42; // Now it's a number
@@ -13,7 +15,7 @@ let names: string[] = ["Alice", "Bob", "Charlie"];
 let flags: boolean[] = [true, false, true];
 
 // tuples
-let person: [string, number] = ["Alice", 30];
+let student: [string, number] = ["Alice", 30];
 
 // enums
 enum Color {
@@ -22,7 +24,32 @@ enum Color {
     BLUE
 }
 
+let favoriteColor: Color = Color.GREEN;
 
-console.log(`Name: ${name}, Age: ${age}, Is Student: ${isStudent}`);
+//object
+let person : { name: string; age: number; } = {
+    name: "Alice",
+    age: 30,
+}
+
+//interface
+interface User{
+    name: string
+    email: string
+    age?: number // this is optional
+}
+
+// using the interface
+let user_data1: User = {
+    name: "Bob",
+    email: "bob@gmail.com"
+}
+
+let user_data2 : User={
+    name: "Ann",
+    email: "ann@gmail.com",
+    age: 28
+}
+
 
 // npx tsc 
