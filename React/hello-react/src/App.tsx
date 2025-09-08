8a7f3f4edfb844342b77602cac7c69208345af9b
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-08 15:45:54 +0530

    Add DetailCard component and update App to use Fragment for multiple elements

--- React/hello-react/src/App.tsx
+++ React/hello-react/src/App.tsx
@@ -1,6 +1,36 @@
+import { Fragment } from "react/jsx-runtime";
+import DetailCard from "./components/Details";
+
 const App = () => {
+  let me = "John Doe";
   // This is a simple React component === jsx -> JavaScript XML
-  return <div>Hello, React!</div>;
+  return (
+    // JSX allows us to write HTML-like syntax in our JavaScript code
+
+    // Fragment is used to group multiple elements without adding extra nodes to the DOM
+    // Fragment is works like a ghost element, it does not render anything in the DOM
+
+    //<></> is a shorthand for <React.Fragment></React.Fragment>
+
+    // we can use javascript expressions inside JSX using {}
+    // we can use self-closing tags for elements that do not have children
+    // <img />, <input />, <br />, <hr />, <meta />, <link />
+   
+  <Fragment>
+    <DetailCard/>
+      <h1>Hello, React!</h1>
+      <h2>My name is {me}</h2>
+      <p>Welcome to your first React application.</p>
+  </Fragment>
+
+  // we cant return multiple elements, we need to wrap them in a single parent element
+  // React.Fragment can be used as a parent element without adding extra nodes to the DOM
+  // <React.Fragment></React.Fragment>
+  //   <div>Hello, React!</div>
+  //   <div>Another element</div>
+  // </React.Fragment>
+);
 }
 
+// Export the component to be used in other parts of the application
 export default App;
---
+++ React/hello-react/src/components/Details.tsx
@@ -0,0 +1,12 @@
+const DetailCard = () => {
+    return <div>
+        <h1>Detail Card Component</h1>
+        <p>This is a detail card component.</p>
+    </div>;
+}
+
+// component must be start with captial letter beacuse we mismatches with HTML tags
+// React components must be capitalized
+// React components are functions that return JSX
+// Export the component to be used in other parts of the application
+export default DetailCard;
