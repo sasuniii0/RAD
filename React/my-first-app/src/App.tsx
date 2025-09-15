970abbd162ec3676b78c15198b2ad7ee2bb584c7
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-15 10:17:51 +0530

    Refactor App component to include a class-based version; update export statement

--- React/my-first-app/src/App.tsx
+++ React/my-first-app/src/App.tsx
@@ -1,5 +1,17 @@
+// Component - functional componant
 function App(){
+  // jsx --> javascript xml(js/ts + html + xml)
+  // tsx 
   return <h1>Hello</h1>
 }
 
-export default App;
+import { Component, type ReactNode } from "react";
+
+// Class Component --. create class in JS and use inheritance
+class AppClass extends  Component{
+  render(): ReactNode {
+    return <h1>Class Component</h1>
+  }
+}
+
+export default AppClass;
