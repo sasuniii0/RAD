28b81d1b543c54623318e9d2efad43b901aac997
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-15 11:29:52 +0530

    Add MyComponent and refactor App to include class component; enhance rendering logic

--- React/my-first-app/src/App.tsx
+++ React/my-first-app/src/App.tsx
@@ -1,17 +1,41 @@
-// Component - functional componant
-function App(){
-  // jsx --> javascript xml(js/ts + html + xml)
-  // tsx 
-  return <h1>Hello</h1>
-}
-
-import { Component, type ReactNode } from "react";
+import { Component, Fragment, type ReactNode } from "react";
 
 // Class Component --. create class in JS and use inheritance
 class AppClass extends  Component{
   render(): ReactNode {
-    return <h1>Class Component</h1>
+    return <Fragment>Class Component</Fragment>
   }
 }
 
-export default AppClass;
+import MyComponent from "./components/MyComponent"
+
+// Component - functional componant
+function App(){
+
+  let user : String ="Sasuni"
+  // jsx --> javascript xml(js/ts + html + xml)
+  // tsx 
+  return (
+    // we can use empty tags to define -- extra element ekk widihata add wenne na me empty tag ek
+    // and also we can use <Fragment></Fragment> --> Fragment is Old--> new version ek thmai <></> empty tags
+    <>
+      < MyComponent />
+      < MyComponent />
+      < MyComponent />
+      < MyComponent />
+      < MyComponent />
+
+      <hr/>
+      <h1> React Functional-Component</h1>
+      <h3>Hello, {user}</h3>
+      {user ? <h4 className="title" style={{color: "red"}}>Hello, {user}</h4> : <h1>Hello, User</h1>}
+      <p>
+        In React, function components are a fundamental way to define components. They are lightweight, simple, and can handle both UI rendering and logic. The react/function-component-definition rule in ESLint enforces consistent function types for defining these components, ensuring code readability and maintainability.
+      </p>
+    </>
+  )
+}
+
+
+
+export default App;
---
+++ React/my-first-app/src/components/MyComponent.tsx
@@ -0,0 +1,11 @@
+const MyComponent = () =>{
+    return (
+<>
+        <h5>Hello, React</h5>
+        <h6>This is my First-Component</h6>
+    </>
+    )
+    
+}
+
+export default MyComponent;
