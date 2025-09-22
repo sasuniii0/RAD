4af90936a2666a24d58767475eed08064b5b2e3a
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-22 10:40:54 +0530

    examples of dependency arrays

--- React/react-day3/src/App.tsx
+++ React/react-day3/src/App.tsx
@@ -6,20 +6,37 @@ export default function App(){
   // useEffect(function,[])
 
   const [count,setCount] = useState(0)
+  const [value,setValue] = useState(0)
+
+  // ===================dependency arrays=======================
+
+  // useEffect(()=>{
+  //   console.log("Hellooo");
+  // },[]) 
+  // if dependency array is empty we can call api calls in there...
+
+  // useEffect(()=>{
+  //   console.log("Hellooo");
+  // },[count]) 
+  // count kyla value eka change wena nisa use status eka run wenwa...
+
+  //console.log("Hiiii");
+
+
+  // useEffect(()=>{
+  //   console.log("Hellooo");
+  // },[value])
 
   useEffect(()=>{
     console.log("Hellooo");
-    
-  },[]) // dependency arrays...
-  // if dependency array is empty we can call api calls in there...
-
-  //console.log("Hiiii");
+  },[count,value])
   
   return(
     <div>
       <h4>{count}</h4>
       <button onClick={()=> setCount((prev) => prev + 1)}>+</button>
       <button onClick={() =>setCount((prev) => prev - 1)}>-</button>
+      <button onClick={() =>setValue((prev) => prev - 1)}>Change Value</button>
     </div>
   )
 }
