9eab3653d2761d21f47c4f5dc8da7994f55f5c2c
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-22 09:39:42 +0530

    class component life cycle

---
+++ React/my-first-app/src/components/ClassComponent.tsx
@@ -0,0 +1,30 @@
+import { Component, type ReactNode } from "react"; 
+
+
+export class ClassComponent extends Component{
+
+    // component life cycle....
+
+    componentDidMount(){
+        // component mounting process...
+        console.log("component mounting...");
+    } 
+
+    componentDidUpdate(prevProps, prevState, snapshot): void {
+        
+    }
+
+    componentWillUnmount(): void {
+        // component unmounting process...
+        console.log("component unmounting....");
+        
+    }
+
+    render(): ReactNode {
+        return(
+            <div>
+                <h1>I'm class component</h1>
+            </div>
+        )
+    }
+}
