f2b4c4a248e67ebd0a8b5de6033335b781c55803
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-22 10:19:01 +0530

    functional component life-cycle explanation with example

--- React/react-day3/src/App.tsx
+++ React/react-day3/src/App.tsx
@@ -1,1 +1,24 @@
 //rfc--> can create functions automatically..
+
+import React , {useEffect, useState} from "react"
+
+export default function App(){
+  // useEffect(function,[])
+
+  const [count,setCount] = useState(0)
+
+  useEffect(()=>{
+    console.log("Hellooo");
+    
+  })
+
+  //console.log("Hiiii");
+  
+  return(
+    <div>
+      <h4>{count}</h4>
+      <button onClick={()=> setCount((prev) => prev + 1)}>+</button>
+      <button onClick={() =>setCount((prev) => prev - 1)}>-</button>
+    </div>
+  )
+}
