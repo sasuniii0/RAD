6678779d37dc4ac682f274d2340f33c7c5500e09
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-29 09:31:19 +0530

    useRef exmplanation with the example of using dom elements

--- React/reactDay4/src/App.tsx
+++ React/reactDay4/src/App.tsx
@@ -1,5 +1,39 @@
-const App = () =>{
+import { useRef , useEffect} from "react"
 
+const App = () =>{
+  const inputRef = useRef<any>(null)
+  console.log(inputRef);
+  
+
+  // when clicking the button the input will be focus in the ui
+  const handleClick =()=>{
+    inputRef?.current.focus()
+
+    // change the background color of the input field using useRef axample...
+    // api methana null da kyla check krla blanna onee.. initialize wenne anthimta nisa..
+    if(inputRef.current){
+      inputRef.current.style.backgroundColor = "red"
+    }
+  }
+  // // default value for this...(userRef)
+  // console.log(ref);// value is in ref.current..
+
+  // useEffect(()=>{
+  //   console.log("Component is re-rendering");
+  // })
+
+  // const handleClick = ()=>{
+  //   ref.current += 1
+  //   console.log(ref.current);
+    
+  // }
+  
+    return(
+      <div>
+        <input ref={inputRef}></input>
+        <button onClick={handleClick}>Click</button>
+      </div>
+    )
 }
 
 export default App
