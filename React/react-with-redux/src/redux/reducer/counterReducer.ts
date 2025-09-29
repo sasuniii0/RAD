6d620a879dc5eefb49c02ab27afd46da7117e756
sasuniii0 <sasuniwijerathne@gmail.com>
2025-09-29 12:29:24 +0530

    init a project of creating store using redex library

---
+++ React/react-with-redux/src/redux/reducer/counterReducer.ts
@@ -0,0 +1,20 @@
+import { createReducer } from "@reduxjs/toolkit"
+import { decrement, increment, setValue } from "../action/counterAction"
+
+const initialState ={
+    count : 0
+}
+
+export const counterReducer = createReducer(initialState,(builder)=>{
+    builder
+    .addCase(increment, (state:any)=>{
+        state.count += 1
+    })
+    .addCase(decrement, (state:any)=>{
+        state.count += 1
+    })
+    .addCase(setValue,(state:any,action:any)=>{
+        state.count = action.payload
+    })
+
+})
