fc77f9a0eab5f92a846f9e4be48dbd4e4dbb0a1c
sasuniii0 <sasuniwijerathne@gmail.com>
2025-10-09 10:07:39 +0530

    add expressjs library to the project

---
+++ Nodejs/first-app/src/index.ts
@@ -0,0 +1,19 @@
+// console.log('Hello, World!');
+
+// // clg --> the shortcut for the console log...
+// console.log("Hello...my first-app")
+
+//------------------------------------------------------------------------------------
+import express, { Application } from 'express';
+
+// : Application --> it is a type annotation which is used to define the type of the variable app.
+const app:Application = express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello, World! This is my first app using express and typescript.")
+})
+
+// we can check from the callback function that the server is running or not.`
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+})
