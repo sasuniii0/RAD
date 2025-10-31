4b73f26a7e6de2dcc94491255ffeb6249ee160e8
sasuniii0 <sasuniwijerathne@gmail.com>
2025-10-31 15:10:57 +0530

    add bcrypt and jwt security dependencies to the application

--- Nodejs/smart-blog-be/src/controllers/authController.ts
+++ Nodejs/smart-blog-be/src/controllers/authController.ts
@@ -1,7 +1,15 @@
 import { Request,Response } from "express";
 
 export const userRegister = async (req:Request,res:Response)=>{
-    res.send('Register Route');
+    try{
+        const {firstName,lastName,email,password}=req.body; 
+        
+        if(!firstName || !lastName || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+    }catch(err:any){
+        return res.status(500).json({message:"Server Error"});
+    } 
 }
 
 export const userLogin = async (req:Request,res:Response)=>{
@@ -9,7 +17,7 @@ export const userLogin = async (req:Request,res:Response)=>{
 }
 
 export const getUserDetail = (req:Request,res:Response)=>{
-    res.send('Get Details Route')
+
 }
 
 export const adminRegister = async (req:Request,res:Response)=>{
