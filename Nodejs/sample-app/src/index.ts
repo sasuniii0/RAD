34eea4a00cbc37c69b733d301bf7b36cc927c981
sasuniii0 <sasuniwijerathne@gmail.com>
2025-10-31 10:00:47 +0530

    feat: add CORS support to the Node.js application
    - Updated package.json to include `cors` and its TypeScript definitions.
    - Modified index.ts to implement CORS middleware with specific options.
    - Added global middleware for logging requests.
    - Updated package-lock.json to reflect new dependencies.
    - Included necessary license and README files for `cors` and `@types/cors`.

--- Nodejs/sample-app/src/index.ts
+++ Nodejs/sample-app/src/index.ts
@@ -1,8 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+//get the data from the body using json type
+//built in middleware
+app.use(express.json());
+
+//cors middleware
+app.use(cors({
+    origin: ["http://localhost:3000"], // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true
+}));
+
+//global middleware
+app.use((req,res,next)=>{
+    //res.send("Hellow im the Global middleware")
+    next()
+})
+
 app.get("/",(req,res)=>{
+    console.log("Get request has been sent to the server");
     res.send("Hellow... This is node.js backend server");
 })
 
