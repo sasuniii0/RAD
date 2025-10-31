99980d8e5d2052e3444b2ca2a8c07a774905a2dc
sasuniii0 <sasuniwijerathne@gmail.com>
2025-10-31 09:39:31 +0530

    sending a get request

--- Nodejs/sample-app/src/index.ts
+++ Nodejs/sample-app/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import express from 'express';
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("Hellow... This is node.js backend server");
+})
+
+app.listen(5000, () => {
+    console.log('Server is running on port 3000');
+});
