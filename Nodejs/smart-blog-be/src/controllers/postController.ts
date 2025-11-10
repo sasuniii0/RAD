d6ff9d7b737ac8008168430d66d0874da548d28f
sasuniii0 <sasuniwijerathne@gmail.com>
2025-11-10 10:40:19 +0530

    done cloudinary configurations

--- Nodejs/smart-blog-be/src/controllers/postController.ts
+++ Nodejs/smart-blog-be/src/controllers/postController.ts
@@ -2,7 +2,9 @@ import { Request,Response } from "express";
 import { AuthRequest } from "../middlewares/authMiddleware";    
 
 export const savePost = async(req:Request, res:Response)=>{
-   
+//    req.file?.buffer ==> file ek ram eke save wela thiyeddi meken eliyta gnna puluwan
+    
+
 }
 
 export const viewAllPost = async (req:Request,res:Response) =>{
--- Nodejs/smart-blog-be/src/routes/postRoute.ts
+++ Nodejs/smart-blog-be/src/routes/postRoute.ts
@@ -3,11 +3,14 @@ import { authenitcate } from "../middlewares/authMiddleware";
 import { requireRole } from "../middlewares/roleRequiredMiddleware";
 import { savePost,viewAllPost,getMyPost } from "../controllers/postController";
 import { Role } from "../models/userModel";
+import { upload } from "../middlewares/multerMiddleware";
 
 const route = Router();
 
 //protected => admin and author
-route.post("/create",authenitcate,requireRole([Role.ADMIN,Role.AUTHOR]), savePost);
+route.post("/create",authenitcate,requireRole([Role.ADMIN,Role.AUTHOR]),upload.single("image"), savePost);
+// upload.single("image") => form data eke key eke name eka thamai api denna one
+// upload.array kyla gnne files godak gnna one nam..
 
 route.get("/", viewAllPost);
 
